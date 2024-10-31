e45f24a80bb1de0d48cb9d521ca551f19ad7ae2b
Jimmy Zheng <Jimmy.Zheng@stonybrook.edu>
2024-10-31 01:07:09 -0400

    fixed refresh bug

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { ThemeProvider } from './context/ThemeContext';
 function App() {
   return (
     <ThemeProvider>
-      <Router>
+      <Router basename='portfolio'>
         <NavigationBar/>
           <Routes>
           <Route path='/' element={<Home />}></Route>
